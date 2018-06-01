d0c91d80eb74a827cf9eee797cd82a4e36bf897f
Stan Goss <gmixlpixl@gmail.com>
2018-06-01 15:02:41 -0400

    initial commit

---
+++ lib/ruby/exercisms.rb
@@ -0,0 +1,7 @@
+require "ruby/exercisms/version"
+
+module Ruby
+  module Exercisms
+    # Your code goes here...
+  end
+end
---
+++ spec/ruby/exercisms_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ruby::Exercisms do
+  it "has a version number" do
+    expect(Ruby::Exercisms::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
